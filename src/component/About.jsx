018929491bc4875ca93e88e922b5d85544626984
Olayinka Bidemi Ayeni <layinka4dat@gmail.com>
2023-05-19 21:33:35 +0100

    edit about component write up

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -13,18 +13,11 @@ const About = () => {
         <h3 className=' text-[#008dea] md:text-6xl text-3xl font-bold uppercase text-center p-9'>About</h3>
         <div className='bg-[#f4f5f7c0] py-10 md:text-xl  lg:text-4xl text-center shadow-sm text-[#5d656b] flex flex-col md:py-28 md:px-20 aboutus-div'>
             <span className='mb-9'>
-              I first became thrilled with Software engineering in 2019. I was with a friend and he
-              was telling me about this thing called Python and how it could automate things.
-              As a person who looks for efficiencies, I was hooked and asked him questions
-              about how I could get started. My friend told me it would be best if I start learning
-              the basics of frontend languages before getting into backend development. I
-              jumped right into HTML & CSS. Soon I was building cool frontend projects. I also
-              added javaScript to sharpen my frontend skills.
-            </span>
+            I am a skilled software developer with expertise in multiple platforms for building websites and web applications.
+            I am an avid reader with a strong work ethic.
+             </span>
             <span>
-              I'm currently seeking a full-time role in Software Engineering, Frontend Engineering
-              and any other related fields where I can contribute my skills both in coding and
-              business to help a company achieve their goals.
+            Through my experience in the field, I have developed a strong understanding of the development process and the ability to deliver high-quality solutions. My portfolio showcases a selection of my work and demonstrates my technical skills and experience. I am excited to use my skills to help bring your ideas to life.
             </span>
         </div>
       </div>
